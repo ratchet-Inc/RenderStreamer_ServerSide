3d9213de9bd83214416827704c76b98adaaadd15
Deniro Gayle <deniroturboman@hotmail.com>
2021-01-07 19:54:10 -0500

    partial dev code for frame streaming

--- fetchFrame.php
+++ fetchFrame.php
@@ -1,11 +1,45 @@
 <?php
 
-require_once("API/DB_Interface.php");
 header("Content-Type: text/json");
+require_once("API/Constants.php");
+require_once("API/DB_Interface.php");
+
+define("SESSION_NAME", "RenderStream", false);
 
 session_start();
-$frame = GetNextFrame();
-echo $frame;
+if(isset($_SESSION[SESSION_NAME][R_CACHE_NAME]) == false){
+	$info = GetCacheInfo(True);
+	$res = $info->status;
+	if($res == 200){
+		$index = -1;
+		$cacheInfo = $info->res->info;
+		// run loop to check which stream we are looking for...
+		// for now there is only 1 stream, so we can just use index 0 at all time.
+		for($i = 0; $i < count($cacheInfo); $i++){
+			if($cacheInfo[$i]->len != 0){
+				$index = $i;
+				break;
+			}
+		}
+		if($index != -1){
+			$obj = new stdClass();
+			$obj->currentFrame = ceil($cacheInfo[$index]->ul / $cacheInfo[$index]->ll);
+			$_SESSION[SESSION_NAME][R_CACHE_NAME] = $obj;
+		}
+	}
+}
+// if for some reason we still dont have any frame count to reference
+if(!isset($_SESSION[SESSION_NAME][R_CACHE_NAME]->currentFrame)){
+	echo '{"msg":"BLANK_FRAME", "status": 500}';
+	return 1;
+}
+$resp = GetNextFrame(True);
+if($resp->status != 200){
+	echo '{"msg":"failed to get frame", "status": 500}';
+	return 0;
+}
+$_SESSION[SESSION_NAME][R_CACHE_NAME]->currentFrame++;
+echo $resp;
 
 return 0;
 
