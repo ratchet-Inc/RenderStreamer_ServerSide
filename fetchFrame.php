765e4eadaeb5f970fa93bf9d30e400fe4b39be61
Deniro Gayle <deniroturboman@hotmail.com>
2021-01-08 19:39:51 -0500

    functional frame streaming

--- fetchFrame.php
+++ fetchFrame.php
@@ -8,22 +8,27 @@ define("SESSION_NAME", "RenderStream", false);
 
 session_start();
 if(isset($_SESSION[SESSION_NAME][R_CACHE_NAME]) == false){
-	$info = GetCacheInfo(True);
+	$info = GetCacheInfo();
 	$res = $info->status;
 	if($res == 200){
 		$index = -1;
 		$cacheInfo = $info->res->info;
 		// run loop to check which stream we are looking for...
-		// for now there is only 1 stream, so we can just use index 0 at all time.
+		// for now there is only 1 stream, so we can just use the first one we see.
 		for($i = 0; $i < count($cacheInfo); $i++){
 			if($cacheInfo[$i]->len != 0){
 				$index = $i;
+				/*echo "upperlimit = ".$cacheInfo[$i]->ul."\n";
+				echo "lowerlimit = ".$cacheInfo[$i]->ll."\n";
+				$mid = $cacheInfo[$i]->ll + ceil(($cacheInfo[$index]->ul - $cacheInfo[$index]->ll) / 2);
+				echo "mid point: $mid\n";*/
 				break;
 			}
 		}
 		if($index != -1){
 			$obj = new stdClass();
-			$obj->currentFrame = ceil($cacheInfo[$index]->ul / $cacheInfo[$index]->ll);
+			$mid = $cacheInfo[$i]->ll + ceil(($cacheInfo[$index]->ul - $cacheInfo[$index]->ll) / 2);
+			$obj->currentFrame = $mid;
 			$_SESSION[SESSION_NAME][R_CACHE_NAME] = $obj;
 		}
 	}
@@ -33,13 +38,20 @@ if(!isset($_SESSION[SESSION_NAME][R_CACHE_NAME]->currentFrame)){
 	echo '{"msg":"BLANK_FRAME", "status": 500}';
 	return 1;
 }
-$resp = GetNextFrame(True);
+$lowL = $_SESSION[SESSION_NAME][R_CACHE_NAME]->currentFrame;
+$uppL = $_SESSION[SESSION_NAME][R_CACHE_NAME]->currentFrame + 1;
+$resp = GetNextFrame($lowL, $uppL);
 if($resp->status != 200){
 	echo '{"msg":"failed to get frame", "status": 500}';
 	return 0;
 }
+if(count($resp->res) == 0){
+	unset($_SESSION[SESSION_NAME][R_CACHE_NAME]);
+	echo '{"msg":"buffer info request queued", "status": 200}';
+	return 0;
+}
 $_SESSION[SESSION_NAME][R_CACHE_NAME]->currentFrame++;
-echo $resp;
+echo json_encode($resp);
 
 return 0;
 
