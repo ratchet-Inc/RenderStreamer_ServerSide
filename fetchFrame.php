15a6cd80ea4e6192b9a8dcf4a5b2344154ac152f
Deniro Gayle <deniroturboman@hotmail.com>
2021-01-18 23:57:18 -0500

    updated script to avoid session usage

--- fetchFrame.php
+++ fetchFrame.php
@@ -5,10 +5,13 @@ require_once("API/Constants.php");
 require_once("API/DB_Interface.php");
 
 define("SESSION_NAME", "RenderStream", false);
-
-session_start();
-if(isset($_SESSION[SESSION_NAME][R_CACHE_NAME]) == false){
+$lowerBound = -1;
+if(isset($_POST['frame'])){
+	$lowerBound = intval($_POST["frame"]);
+}
+if($lowerBound < 1){
 	$info = GetCacheInfo();
+	//print_r($info);
 	$res = $info->status;
 	if($res == 200){
 		$index = -1;
@@ -25,28 +28,27 @@ if(isset($_SESSION[SESSION_NAME][R_CACHE_NAME]) == false){
 			$obj = new stdClass();
 			$mid = $cacheInfo[$i]->ll + ceil(($cacheInfo[$index]->ul - $cacheInfo[$index]->ll) / 2);
 			$obj->currentFrame = $mid;
-			$_SESSION[SESSION_NAME][R_CACHE_NAME] = $obj;
+			$lowerBound = $obj->currentFrame;
 		}
 	}
 }
 // if for some reason we still dont have any frame count to reference
-if(!isset($_SESSION[SESSION_NAME][R_CACHE_NAME]->currentFrame)){
+if($lowerBound < 1){
 	echo '{"msg":"BLANK_FRAME", "status": 500}';
 	return 1;
 }
-$lowL = $_SESSION[SESSION_NAME][R_CACHE_NAME]->currentFrame;
-$uppL = $_SESSION[SESSION_NAME][R_CACHE_NAME]->currentFrame + 12;
+$lowL = $lowerBound;
+$uppL = $lowerBound + 12;
 $resp = GetNextFrame($lowL, $uppL);
+//print_r($resp);
 if($resp->status != 200){
 	echo '{"msg":"failed to get frame", "status": 500}';
 	return 0;
 }
 if(count($resp->res) == 0){
-	unset($_SESSION[SESSION_NAME][R_CACHE_NAME]);
 	echo '{"msg":"buffer info request queued", "status": 200}';
 	return 0;
 }
-$_SESSION[SESSION_NAME][R_CACHE_NAME]->currentFrame = $resp->res[count($resp->res)]->_;
 echo json_encode($resp);
 
 return 0;
